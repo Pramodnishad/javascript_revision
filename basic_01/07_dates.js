8a335ed22653a3544a4983f5fe875d5dc91cb27c
pramodfoodee <nishadpramod.001@gmail.com>
2024-04-11 01:29:19 +0530

    😶‍🌫️ dates section

--- basic_01/07_dates.js
+++ basic_01/07_dates.js
@@ -9,3 +9,27 @@ let myDate = new Date()
 // console.log(myDate.toLocaleDateString())
 // console.log(myDate.toLocaleString())
 // console.log(myDate.getMonth())
+// console.log(typeof myDate)
+
+// let myCreatedData = new Date(2024, 0, 26)
+// console.log(myCreatedData.toLocaleDateString())
+let myCreatedData = new Date("10-4-2024")
+// console.log(myCreatedData.toLocaleString())
+
+let timeStamp = Date.now()
+
+// console.log(Math.floor(Date.now()/1000))
+
+
+let newDate = new Date()
+// console.log(newDate)
+// console.log(newDate.getMonth() + 1)
+// console.log(newDate.getDay())
+
+newDate.toLocaleString(
+    'default',
+    {
+        weekday:"long"
+    }
+)
+// console.log(newDate)
