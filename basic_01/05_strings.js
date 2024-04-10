13645e1fe9021e2ecaa9d68c0dae5e99f52b24e5
pramodfoodee <nishadpramod.001@gmail.com>
2024-04-10 16:40:51 +0530

    🌿 numbers and maths

---
+++ basic_01/05_strings.js
@@ -0,0 +1,30 @@
+const name = "Pramod"
+
+const gitRepoCount = 20
+
+// console.log(name + " " +  gitRepoCount)
+
+// console.log(`My name is  ${name} and my git repo count is ${gitRepoCount }`)
+
+const gameName = new String("Pramod-Nishad")
+
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(5))
+// console.log(gameName.indexOf('d'))
+
+const newString = gameName.substring(0,6)
+// console.log(newString)
+
+const anotherString = gameName.slice(0,6)
+// console.log(anotherString)
+
+
+const newStringOne = "         Prmaod           "
+// console.log(newStringOne)
+
+// console.log(newStringOne.trim())
+
+const url = "https://pramod.com/kumar%20nishad"
+// console.log(url.replace('%20','*'))
+// console.log(url.includes('ramana'))
+console.log(gameName.split('-'))
