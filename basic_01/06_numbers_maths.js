8cecd89a5471c9d2476c5ca42d18a5ba6171f3c6
pramodfoodee <nishadpramod.001@gmail.com>
2024-04-10 16:54:11 +0530

    🧵 date

--- basic_01/06_numbers_maths.js
+++ basic_01/06_numbers_maths.js
@@ -33,8 +33,9 @@ const hundreds = 1000000.547855
 
 // console.log(Math.random())
 // console.log(Math.floor(Math.random()*10)+1)
+// console.log(Math.floor(Math.random()*100)+1)
 
 const min = 1
 const max = 6
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
