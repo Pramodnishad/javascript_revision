553a76e78e5c41b0606f637763856114b56f0dce
pramodfoodee <nishadpramod.001@gmail.com>
2024-04-10 09:27:03 +0530

    👑 conversionoperations part is completed

---
+++ 02_datatypes.js
@@ -0,0 +1,19 @@
+// alert (3 + 3) we are using nodejs not browser
+
+let name = "pramod";
+let age = 25;
+let userLoggedIn = true;
+
+
+// string => ""
+// number   
+// boolean => true or false
+// bigInt
+// null => stand alone value
+// undefined =>
+//  symbol
+
+//  object 
+
+console.log(typeof null)   
+// output will be object
---
+++ 03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score = 50;
+// let score = null;
+let score = undefined;
+// let score = "50abc";
+
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" =>33;
+// "33abc" =>NaN;
+// true =>1; false =>0
+
+
+let isLoggedIn = 0;
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn)
+
+let someNumber = 100;
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
